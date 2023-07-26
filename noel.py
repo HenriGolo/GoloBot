6b95a8166fee34de754844f31264d5e5a576524f
Henri Golo <henri.golo.313f@gmail.com>
2023-07-26 08:24:39 +0200

    messages de bonne année et joyeux noel

---
+++ noel.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import discord
+import time, datetime
+import subprocess
+import inspect
+from infos import token
+
+intents = discord.Intents.all()
+bot = discord.Bot(intents=intents)
+logs = open('logs/logs_dev.txt', 'a')
+
+@bot.event
+async def on_ready():
+	currentTime = datetime.datetime.strptime(time.ctime(),"%c")
+	for guild in bot.guilds:
+		if guild.id == 664006363508244481:
+			continue
+		await guild.system_channel.send("Joyeux Noël :partying_face:")
+		logs.write(f"\n{currentTime} Joyeux Noël souhaité dans {guild}\n")
+	await bot.close()
+
+# bot launch
+bot.run(token=token)
+logs.close()
