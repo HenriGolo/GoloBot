86ddaf72fa24c5ba6be49f72fd438fc2394d1ab1
Henri Golo <henri.golo.313f@gmail.com>
2023-07-26 08:24:56 +0200

    redémarrage du bot

---
+++ restart.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from subprocess import Popen, DEVNULL
+import infos
+
+# ~ __file__ contient le chemin et le nom du fichier
+# ~ Sépare au niveau des / pour récupérer tous les dossiers et le fichier
+# ~ Suppression du nom du fichier en conservant tout sauf le dernier élément
+# ~ Recollage des morceaux avec "/".join()
+pwd = "/".join(__file__.split('/')[:-1])
+Popen([infos.bot],
+		stdin=DEVNULL,
+		stdout=open(f'{pwd}/{infos.stdout}', 'a'),
+		stderr=open(f'{pwd}/{infos.stderr}', 'a'),
+		start_new_session=True,
+		shell=True)
