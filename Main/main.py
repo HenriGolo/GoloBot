125a5bcf4c42d5936b88428c1aa5078e13161680
Henri Golo <henri.golo.313f@gmail.com>
2023-08-19 15:00:23 +0200

    aération code + correction oubli

--- Main/main.py
+++ Main/main.py
@@ -8,12 +8,16 @@ bot = Bot(intents=intents)
 @bot.event
 async def on_ready():
 	currentTime, _, _ = await init(bot)
+
 	bot.startTime = currentTime
+
 	# ~ Récupération du chemin du fichier
 	bot.pwd = "/".join(__file__.split('/')[:-1])+"/"
+
 	# ~ Création de session pour les requêtes
 	bot.session = CustomSession()
-	# ~ Lecture les bot.stats sérialisées
+
+	# ~ Lecture les stats sérialisées
 	bot.stats = Stats()
 	bot.stats.read(infos.stats)
 	bot.stats = bot.stats
@@ -21,31 +25,41 @@ async def on_ready():
 	bot.players = dict()
 	for joueur in bot.stats.joueurs:
 		bot.players[joueur.name] = joueur
+
 	# ~ Récupération de l'User du dev
 	bot.dev = await bot.fetch_user(infos.ownerID)
+
 	bot.lastDM = bot.dev
+
 	bot.games = dict()
+
 	# ~ Création de la bot.whitelist des User avec des permissions sur le bot
 	wl = infos.whitelisted_users
 	bot.whitelist = [bot.dev]
 	for id in wl:
 		bot.whitelist.append(await bot.fetch_user(id))
+
+	# ~ Message de statut du bot
 	activity = Activity(name="GitHub",
 						state="https://github.com/HenriGolo/GoloBot",
 						type=ActivityType.watching)
 	await bot.change_presence(activity=activity)
+
+	# ~ View persistantes
 	bot.add_view(ViewRoleReact())
+
+	# ~ Gestion pour pid pour kill proprement
 	with open(infos.pidfile, "w") as file:
 		file.write(str(getpid()))
 
-	# ~ Ajout commandes
-	bot.add_cog(General(bot))
-	bot.add_cog(Dev(bot))
-	bot.add_cog(Admin(bot))
-	bot.add_cog(Fun(bot))
-	bot.add_cog(WorldOfWarships(bot))
-
 	print(f'Logged on as {bot.user}!')
+
+# ~ Ajout commandes
+bot.add_cog(General(bot))
+bot.add_cog(Dev(bot))
+bot.add_cog(Admin(bot))
+bot.add_cog(Fun(bot))
+bot.add_cog(WorldOfWarships(bot))
 
 # ~ Run
 bot.run(token=infos.tokenDSC)
