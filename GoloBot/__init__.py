db512bb3b211681f532ae7ccc6d5f5a3cc47d505
Henri Golo <henri.golo.313f@gmail.com>
2023-08-19 12:41:08 +0200

    séparation fonctionnalités du bot et procédure de lancement

--- GoloBot/__init__.py
+++ GoloBot/__init__.py
@@ -28,10 +28,6 @@ from GoloBot.Auxilliaire.aux_maths import * # ~ Outils mathématiques
 from GoloBot.WoWs.wowsAPI import * # ~ L'API de World of Warships adaptée pour lisibilité
 import infos # ~ Les tokens c'est privé
 
-# ~ Création Bot
-intents = Intents.all()
-bot = Bot(intents=intents)
-
 # ~ À appeler à chaque commande
 # ~ récupère la date, le Member du bot et le User de l'auteur
 async def init(guild=None, author=None):
@@ -337,37 +333,6 @@ class ViewRoleReact(ui.View):
 		self.add_item(SelectRoleReact(roles))
 
 # ~ Code du bot
-@bot.event
-async def on_ready():
-	currentTime, _, _ = await init()
-	bot.startTime = dt.datetime.strptime(time.ctime(),"%c")
-	# ~ Récupération du chemin du fichier
-	bot.pwd = "/".join(__file__.split('/')[:-1])+"/"
-	# ~ Création de session pour les requêtes
-	bot.session = CustomSession()
-	# ~ Lecture les bot.stats sérialisées
-	bot.stats = Stats()
-	bot.stats.read(infos.stats)
-	bot.stats = bot.stats
-	# ~ Remplissage notre dictionnaire de joueurs
-	bot.players = dict()
-	for joueur in bot.stats.joueurs:
-		bot.players[joueur.name] = joueur
-	# ~ Récupération de l'User du dev
-	bot.dev = await bot.fetch_user(infos.ownerID)
-	bot.lastDM = bot.dev
-	bot.games = dict()
-	# ~ Création de la bot.whitelist des User avec des permissions sur le bot
-	wl = infos.whitelisted_users
-	bot.whitelist = [bot.dev]
-	for id in wl:
-		bot.whitelist.append(await bot.fetch_user(id))
-	await bot.change_presence(activity=Game(name="https://github.com/HenriGolo/GoloBot", start=bot.startTime))
-	bot.add_view(ViewRoleReact())
-	with open(infos.pidfile, "w") as file:
-		file.write(str(getpid()))
-	print(f'Logged on as {bot.user}!')
-
 class General(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
@@ -1140,15 +1105,3 @@ class WorldOfWarships(commands.Cog):
 		except Exception:
 			with open(infos.stderr, 'a') as file:
 				file.write(f"\n{currentTime}\n{fail()}\n")
-
-# ~ Lancement
-def startBot():
-	# ~ Ajout commandes
-	bot.add_cog(General(bot))
-	bot.add_cog(Dev(bot))
-	bot.add_cog(Admin(bot))
-	bot.add_cog(Fun(bot))
-	bot.add_cog(WorldOfWarships(bot))
-
-	# ~ Run
-	bot.run(token=infos.tokenDSC)
--- Main/main.py
+++ Main/main.py
@@ -1,3 +1,51 @@
 #!/usr/bin/env python3
-from GoloBot import startBot
-startBot()
+from GoloBot import * # ~ Contient tout ce qu'il faut, imports compris
+
+# ~ Création Bot
+intents = Intents.all()
+bot = Bot(intents=intents)
+
+@bot.event
+async def on_ready():
+	currentTime, _, _ = await init()
+	bot.startTime = currentTime
+	# ~ Récupération du chemin du fichier
+	bot.pwd = "/".join(__file__.split('/')[:-1])+"/"
+	# ~ Création de session pour les requêtes
+	bot.session = CustomSession()
+	# ~ Lecture les bot.stats sérialisées
+	bot.stats = Stats()
+	bot.stats.read(infos.stats)
+	bot.stats = bot.stats
+	# ~ Remplissage notre dictionnaire de joueurs
+	bot.players = dict()
+	for joueur in bot.stats.joueurs:
+		bot.players[joueur.name] = joueur
+	# ~ Récupération de l'User du dev
+	bot.dev = await bot.fetch_user(infos.ownerID)
+	bot.lastDM = bot.dev
+	bot.games = dict()
+	# ~ Création de la bot.whitelist des User avec des permissions sur le bot
+	wl = infos.whitelisted_users
+	bot.whitelist = [bot.dev]
+	for id in wl:
+		bot.whitelist.append(await bot.fetch_user(id))
+	activity = Activity(name="GitHub",
+						state="https://github.com/HenriGolo/GoloBot",
+						type=ActivityType.watching)
+	await bot.change_presence(activity=activity)
+	bot.add_view(ViewRoleReact())
+	with open(infos.pidfile, "w") as file:
+		file.write(str(getpid()))
+	print(f'Logged on as {bot.user}!')
+
+# ~ Ajout commandes
+bot.add_cog(General(bot))
+bot.add_cog(Dev(bot))
+bot.add_cog(Admin(bot))
+bot.add_cog(Fun(bot))
+bot.add_cog(WorldOfWarships(bot))
+
+# ~ Run
+bot.run(token=infos.tokenDSC)
+
