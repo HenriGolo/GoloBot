37c922aed6f154b65096ce1d68cfe9d4ddd0b1f0
Henri Golo <henri.golo.313f@gmail.com>
2023-09-12 16:27:38 +0200

    enlèvement de variable inutile

--- Main/restart.py
+++ Main/restart.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python3
 from subprocess import Popen, DEVNULL
-import infos
+from infos import bot
 
-# ~ __file__ contient le chemin et le nom du fichier
-# ~ Sépare au niveau des / pour récupérer tous les dossiers et le fichier
-# ~ Suppression du nom du fichier en conservant tout sauf le dernier élément
-# ~ Recollage des morceaux avec "/".join()
-pwd = "/".join(__file__.split('/')[:-1])
-Popen([infos.bot],
+Popen([bot],
 		stdin=DEVNULL,
 		stdout=open(infos.stdout, 'a'),
 		stderr=open(infos.stderr, 'a'),
