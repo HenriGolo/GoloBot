eb82aea9fed2d871af8fe406b950cbe4cb8d5cdd
Henri Golo <henri.golo.313f@gmail.com>
2023-11-17 22:08:58 +0100

    "avec la pièce jointe, c'est mieux" version commit

--- Main/restart.py
+++ Main/restart.py
@@ -3,12 +3,12 @@ from os import environ
 from subprocess import Popen, DEVNULL
 
 with open(environ['pidfile'], 'r') as file:
-      try:
-            Popen(["kill", file.read()])
-      finally:
-            Popen([environ['bot_path']],
-                  stdin=DEVNULL,
-                  stdout=open(environ['stdout'], 'a'),
-                  stderr=open(environ['stderr'], 'a'),
-                  start_new_session=True,
-                  shell=True)
+    try:
+        Popen(["kill", file.read()])
+    finally:
+        Popen([environ['bot_path']],
+              stdin=DEVNULL,
+              stdout=open(environ['stdout'], 'a'),
+              stderr=open(environ['stderr'], 'a'),
+              start_new_session=True,
+              shell=True)
