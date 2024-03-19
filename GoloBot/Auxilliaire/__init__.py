9ed5234460581be900e3abd2cee9db287b4f3dab
Henri Golo <henri.golo.313f@gmail.com>
2024-03-19 16:22:33 +0100

    fix syntaxe

--- GoloBot/Auxilliaire/__init__.py
+++ GoloBot/Auxilliaire/__init__.py
@@ -94,7 +94,7 @@ class GBEmbed(discord.Embed):
         if isinstance(user, (discord.User, discord.Member)):
             self.set_author(name=user.name, url=user.jump_url, icon_url=user.avatar.url)
 
-    def add_field(self: discord.embeds.E, *, name: str, value: str, inline: bool = False) -> E:
+    def add_field(self: discord.embeds.E, *, name: str, value: str, inline: bool = False) -> discord.embeds.E:
         return super().add_field(name=name, value=value, inline=inline)
 
 
