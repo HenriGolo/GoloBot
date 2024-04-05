b2c9b4cd79bef3d98cd6155fabb0ea49ae27f9a1
Henri Golo <henri.golo.313f@gmail.com>
2024-04-05 19:01:19 +0200

    log du pseudo du propriétaire du serveur

--- Main/main.py
+++ Main/main.py
@@ -102,7 +102,7 @@ class GoloBot(BotTemplate):
         except:
             pass
         with open('logs/guilds.log', 'a') as file:
-            txt = guild.name
+            txt = f'{guild.name} - {guild.owner.name}'
             if isinstance(inv, discord.Invite):
                 txt += f' - {inv.url}'
             file.write(f"{txt}\n")
