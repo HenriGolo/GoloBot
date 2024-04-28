9cd9b70ab88709eda424aeb92ea679bb0900093e
Henri Golo <henri.golo.313f@gmail.com>
2024-04-28 17:03:54 +0200

    fix restart

--- Main/restart.py
+++ Main/restart.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from os import environ
 from subprocess import Popen, DEVNULL
+from GoloBot.Auxilliaire import path
 
 # kill ancien bot, si existant
 with open(path + environ['pidfile'], 'r') as pid:
