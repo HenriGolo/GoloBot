d1bb7c48af8516136a01ffbdc2465d7f537b9e58
Henri Golo <henri.golo.313f@gmail.com>
2024-07-17 15:44:52 +0200

    suppression try

--- Main/restart.py
+++ Main/restart.py
@@ -5,15 +5,11 @@ from GoloBot.Auxilliaire import GBpath
 
 # kill ancien bot, si existant
 with open(GBpath + os.environ['pidfile'], 'r') as pid, open(GBpath + os.environ['stderr'], 'a') as stderr:
-    try:
-        subprocess.Popen(f'kill {pid.read()}', shell=True, stderr=subprocess.DEVNULL)
-    except:
-        pass
-    finally:
-        # lancement du bot
-        subprocess.Popen([GBpath + os.environ['bot_path']],
-                         stdin=subprocess.DEVNULL,
-                         stdout=subprocess.DEVNULL,
-                         stderr=stderr,
-                         start_new_session=True,
-                         shell=True)
+    subprocess.Popen(f'kill {pid.read()}', shell=True, stderr=subprocess.DEVNULL)
+    # lancement du bot
+    subprocess.Popen([GBpath + os.environ['bot_path']],
+                     stdin=subprocess.DEVNULL,
+                     stdout=subprocess.DEVNULL,
+                     stderr=stderr,
+                     start_new_session=True,
+                     shell=True)
